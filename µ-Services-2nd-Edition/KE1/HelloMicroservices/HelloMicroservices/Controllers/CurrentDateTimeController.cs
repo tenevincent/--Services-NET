b550fe68e65c7d33397255064a7f72334f2aa3d1
Vincent Tene <vincent.tene@gmail.com>
2022-07-19 13:08:20 +0200

    first commit

---
+++ µ-Services-2nd-Edition/KE1/HelloMicroservices/HelloMicroservices/Controllers/CurrentDateTimeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMicroservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentDateTimeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public object Get() => DateTime.UtcNow;
+    }
+}
